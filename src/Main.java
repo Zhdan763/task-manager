3197e5e1373bf5eb461b832ca298194cbe6ed768
Zhdan763 <adnrey.zhdanenkov@yandex.ru>
2023-04-12 12:49:00 +0400

    view

--- src/Main.java
+++ src/Main.java
@@ -6,63 +6,36 @@ import model.Journal;
 import model.Task;
 import view.View;
 
+import java.text.ParseException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException, TaskNotFoundException {
         IdGenerator idGenerator = IdGenerator.getIdGenerator();
-        // Controller controller = new Controller();
         Controller controller = Controller.getController();
+        View view = View.getView();
 
-        //  Task task1 = TaskFactory.createTask("aaa", "ss", new Date(123), idGenerator.getNextId());
-        //Task task2 = taskFactory.createTask("bbb", "saas", new Date(123322), idGenerator.getNextId());
-        // Task task2 = TaskFactory.createTask("bbb", "saas", new Date(123322), idGenerator.getNextId());
+
+
         controller.createTask("aa", "ssss", new Date(1222));
         controller.createTask("aaa", "ssss", new Date(1222));
 
+        view.run();
+
+        /*
         try {
             controller.getTask(1);
         } catch (TaskNotFoundException e) {
             e.printStackTrace();
         }
+
         try {
-            controller.getTask(55);
+            controller.getTask(0);
         } catch (TaskNotFoundException e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
         }
         controller.getAllTasks();
-
-
-
-       /*
-        Task model = new Task();
-        model.setTaskName("test");
-        model.setDescription("Call to Nikita");
-        model.setDate(new Date(1234)); //почекать от 1 янв 1970 млс
-        model.setId(idGenerator.getNextId());
-        model.setId(0);
-
-        Task model2 = new Task();
-        model2.setTaskName("privat");
-        model2.setDescription("Call to Andrey");
-        model2.setDate(new Date(22222));
-        model2.setId(idGenerator.getNextId());
-
-        Task model3 = new Task();
-        model3.setTaskName("hp");
-        model3.setDescription("Call to Vi");
-        model3.setDate(new Date(465481));
-        model3.setId(idGenerator.getNextId());
-
-        Journal journal = new Journal();
-        journal.addTask(model);
-        journal.addTask(model2);
-        journal.addTask(model3);
-        //journal.removeTask(1);
-        System.out.println(journal);
-        System.out.println(journal.getTask(2));
-        System.out.println(journal.getAllTasks());
 */
 
     }
--- src/view/View.java
+++ src/view/View.java
@@ -1,10 +1,160 @@
 package view;
 
+import controller.Controller;
+import controller.IdGenerator;
+import exceptions.TaskNotFoundException;
+import model.Task;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class View {
-    public void printInfo(String taskName, String description, int contacts) {
-        System.out.println("Look: ");
-        System.out.println("TaskName: " + taskName);
-        System.out.println("Description: " + description);
-        System.out.println("Contacts: " + contacts);
+    private static View view;
+    private Scanner scanner;
+
+
+    Controller controller = Controller.getController();
+
+    public static View getView() {
+        if (view == null) {
+            view = new View();
+        }
+        return view;
     }
+
+    private View() {
+        this.scanner = new Scanner(System.in);
+
+    }
+
+
+    public void run() throws ParseException, TaskNotFoundException {
+        int menuAction = -1;
+
+        System.out.println("Task Manager");
+        while (menuAction != 5) {
+            menuAction = runMenu();
+            switch (menuAction) {
+                case 1:
+                    showTasks();
+                    break;
+                case 2:
+                    createTask();
+                    break;
+                case 3:
+                    deleteTask();
+                    break;
+                case 4:
+                    modifyTask();
+                    break;
+                case 5:
+                    System.out.println("\n Good bye!");
+                    break;
+                default:
+                    System.out.println("Error: Incorrect value, please choose correct");
+                    break;
+            }
+        }
+    }
+
+    private int runMenu() {
+        System.out.println();
+        System.out.println("Choose action:");
+        System.out.println("1. Display all tasks");
+        System.out.println("2. Create tasks");
+        System.out.println("3. Delete task");
+        System.out.println("4. Modify task");
+        System.out.println("5. Exit");
+
+        System.out.print("\nAction: ");
+        String input = scanner.nextLine();
+        return Integer.valueOf(input);
+    }
+
+    private void showTasks() {
+        System.out.println("Tasks: ");
+        for (Task i : controller.getAllTasks()) {
+            System.out.println(i);
+        }
+
+        controller.getAllTasks();
+        System.out.print("Press \"Enter\" for return to the main menu...\n");
+        scanner.nextLine();
+    }
+
+    private void createTask() throws ParseException {
+        System.out.println("\nNew Task:");
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter description: ");
+        String description = scanner.nextLine();
+        Date date;
+        while (true) {
+            System.out.print("Enter date (yyyy-mm-dd): ");
+            String dateAa = scanner.nextLine();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd", Locale.ENGLISH);
+            try {
+                date = formatter.parse(dateAa);
+                System.out.println("Correct date");
+                break;
+            } catch (ParseException e) {
+                System.out.println("Input is not a date, continue");
+            }
+        }
+        controller.createTask(name, description, date);
+        System.out.println("\nInfo: Task has been added!");
+
+    }
+
+
+    private void deleteTask() {
+        System.out.println("What task do you want to delete?");
+        System.out.print("\nEnter id: ");
+        // String id = scanner.nextLine();
+        // Integer id2 = Integer.valueOf(id);
+        //Integer idForDel = id2;
+        //controller.deleteTask(idForDel);
+        // System.out.println("Task number "+idForDel+" has been deleted");
+        String id = scanner.nextLine();
+        int id2 = Integer.valueOf(id);
+        controller.deleteTask(id2);
+        System.out.println("Task number " + id2 + " has been deleted");
+    }
+
+    private void modifyTask() throws TaskNotFoundException {
+        System.out.println("What task do you want to change?");
+        System.out.print("\nEnter id: ");
+        String id = scanner.nextLine();
+        int id2 = Integer.valueOf(id);
+
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter description: ");
+        String description = scanner.nextLine();
+
+        Date date;
+        while (true) {
+            System.out.print("Enter date (yyyy-mm-dd): ");
+            String dateAa = scanner.nextLine();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd", Locale.ENGLISH);
+            try {
+                date = formatter.parse(dateAa);
+                System.out.println("Correct date");
+                break;
+            } catch (ParseException e) {
+                System.out.println("Input is not a date, continue");
+            }
+        }
+
+       controller.updateTask(controller.getTask(id2));
+
+    }
+
+
 }
+
